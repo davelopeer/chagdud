4ed3e2a0e89452ac8bd56db75937c8cc21c09365
David Barenco <davidbarenco@gmail.com>
2020-08-01 18:17:14 -0300

    Create Khadro Ling about us page.

--- khadro_ling/urls.py
+++ khadro_ling/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from khadro_ling.views import KhadroLingHomeView
+from khadro_ling.views import KhadroLingHomeView, KhadroLingAboutUsView
 
 
 app_name = 'khadroling'
 
 urlpatterns = [
     path('', KhadroLingHomeView.as_view(), name='home'),
+    path('quem-somos', KhadroLingAboutUsView.as_view(), name='about-us'),
 ]
--- khadro_ling/views.py
+++ khadro_ling/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class KhadroLingHomeView(TemplateView):
     template_name = 'khadro_ling/home.html'
+
+class KhadroLingAboutUsView(TemplateView):
+    template_name = 'khadro_ling/about_us.html'
