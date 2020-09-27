db9d1638fc89861e732540465b3e751e206feaf1
David Barenco <david.barenco@neoprospecta.com>
2020-09-26 22:39:10 -0300

    Make general changes in the html and css. (part 2)

--- chagdud/settings.py
+++ chagdud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
@@ -76,10 +77,19 @@ WSGI_APPLICATION = 'chagdud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# import pymysql
+
+# pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'HOST': 'localhost',
+        # 'NAME': 'chagdud',
+        # 'USER': 'root',
+        # 'PASSWORD': 'mysql',
     }
 }
 
