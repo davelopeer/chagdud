7f0f514a5d78e55d350bdc77b0c8050bc0c46d97
David <david.contage@intelbras.com.br>
2021-08-15 19:10:24 -0300

    fix events error by templating tag event name

--- khadro_ling/views.py
+++ khadro_ling/views.py
@@ -9,6 +9,12 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['events'] = Event.events_to_come.all()
+
+        if 'django_language' in self.request.COOKIES.keys():
+          context['user_language'] = self.request.COOKIES['django_language']
+        else:
+          context['user_language'] = 'pt'
+
         return context
 
 
